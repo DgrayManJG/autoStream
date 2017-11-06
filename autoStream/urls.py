a7a8ffb116a1755e6f059ce09220ecf0a1739033
GUEVEL Jimmy <jimmy.guevel2016@campus-eni.fr>
2017-11-06 14:20:11 +0100

    ajout html dans blog

--- autoStream/urls.py
+++ autoStream/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('blog.urls')),
     url(r'^streaming/', include('streaming.urls')),
     url(r'^admin/', admin.site.urls),
 ]
